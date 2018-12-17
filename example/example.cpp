60902c96c7c3ee2dc6b63a44665a1ea59f28b1e2
Udi Trugman <udi@trugman.com>
2018-12-17 15:37:19 +0200

    add syslog_udp_sink

--- example/example.cpp
+++ example/example.cpp
@@ -10,23 +10,44 @@
 #include <cstdio>
 
 void stdout_logger_example();
+
 void basic_example();
+
 void rotating_example();
+
 void daily_example();
+
 void async_example();
+
 void binary_example();
+
 void trace_example();
+
 void multi_sink_example();
+
 void user_defined_example();
+
 void err_handler_example();
+
 void syslog_example();
+
 void clone_example();
+
+void replaceDefaultLogger();
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "spdlog/spdlog.h"
 
-int main(int, char *[])
-{
+int main(int, char *[]) {
     spdlog::info("Welcome to spdlog version {}.{}.{} !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+
+    replaceDefaultLogger();
+
     spdlog::warn("Easy padding in numbers like {:08d}", 12);
     spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
     spdlog::info("Support for floats {:03.2f}", 1.23456);
@@ -44,8 +65,7 @@ int main(int, char *[])
     spdlog::info("This an info message with custom format");
     spdlog::set_pattern("%+"); // back to default format
 
-    try
-    {
+    try {
         stdout_logger_example();
         basic_example();
         rotating_example();
@@ -70,18 +90,17 @@ int main(int, char *[])
         spdlog::shutdown();
     }
 
-    // Exceptions will only be thrown upon failed logger or sink construction (not during logging).
-    catch (const spdlog::spdlog_ex &ex)
-    {
+        // Exceptions will only be thrown upon failed logger or sink construction (not during logging).
+    catch (const spdlog::spdlog_ex &ex) {
         std::printf("Log initialization failed: %s\n", ex.what());
         return 1;
     }
 }
 
 #include "spdlog/sinks/stdout_color_sinks.h"
+
 // or #include "spdlog/sinks/stdout_sinks.h" if no colors needed.
-void stdout_logger_example()
-{
+void stdout_logger_example() {
     // Create color multi threaded logger.
     auto console = spdlog::stdout_color_mt("console");
     // or for stderr:
@@ -89,45 +108,43 @@ void stdout_logger_example()
 }
 
 #include "spdlog/sinks/basic_file_sink.h"
-void basic_example()
-{
+
+void basic_example() {
     // Create basic file logger (not rotated).
     auto my_logger = spdlog::basic_logger_mt("file_logger", "logs/basic-log.txt");
 }
 
 #include "spdlog/sinks/rotating_file_sink.h"
-void rotating_example()
-{
+
+void rotating_example() {
     // Create a file rotating logger with 5mb size max and 3 rotated files.
     auto rotating_logger = spdlog::rotating_logger_mt("some_logger_name", "logs/rotating.txt", 1048576 * 5, 3);
 }
 
 #include "spdlog/sinks/daily_file_sink.h"
-void daily_example()
-{
+
+void daily_example() {
     // Create a daily logger - a new file is created every day on 2:30am.
     auto daily_logger = spdlog::daily_logger_mt("daily_logger", "logs/daily.txt", 2, 30);
 }
 
 // Clone a logger and give it new name.
 // Useful for creating component/subsystem loggers from some "root" logger.
-void clone_example()
-{
+void clone_example() {
     auto network_logger = spdlog::default_logger()->clone("network");
     network_logger->info("Logging network stuff..");
 }
 
 #include "spdlog/async.h"
-void async_example()
-{
+
+void async_example() {
     // Default thread pool settings can be modified *before* creating the async logger:
     // spdlog::init_thread_pool(32768, 1); // queue with max 32k items 1 backing thread.
     auto async_file = spdlog::basic_logger_mt<spdlog::async_factory>("async_file_logger", "logs/async_log.txt");
     // alternatively:
     // auto async_file = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>("async_file_logger", "logs/async_log.txt");
 
-    for (int i = 1; i < 101; ++i)
-    {
+    for (int i = 1; i < 101; ++i) {
         async_file->info("Async message #{}", i);
     }
 }
@@ -142,11 +159,10 @@ void async_example()
 // {:n} - don't split the output to lines.
 
 #include "spdlog/fmt/bin_to_hex.h"
-void binary_example()
-{
+
+void binary_example() {
     std::vector<char> buf;
-    for (int i = 0; i < 80; i++)
-    {
+    for (int i = 0; i < 80; i++) {
         buf.push_back(static_cast<char>(i & 0xff));
     }
     spdlog::info("Binary example: {}", spdlog::to_hex(buf));
@@ -159,8 +175,7 @@ void binary_example()
 
 // Compile time log levels.
 // define SPDLOG_ACTIVE_LEVEL to required level (e.g. SPDLOG_LEVEL_TRACE)
-void trace_example()
-{
+void trace_example() {
     // trace from default logger
     SPDLOG_TRACE("Some trace message.. {} ,{}", 1, 3.23);
     // debug from default logger
@@ -172,8 +187,7 @@ void trace_example()
 }
 
 // A logger with multiple sinks (stdout and file) - each with a different format and log level.
-void multi_sink_example()
-{
+void multi_sink_example() {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     console_sink->set_level(spdlog::level::warn);
     console_sink->set_pattern("[multi_sink_example] [%^%l%$] %v");
@@ -189,37 +203,38 @@ void multi_sink_example()
 
 // User defined types logging by implementing operator<<
 #include "spdlog/fmt/ostr.h" // must be included
-struct my_type
-{
+
+struct my_type {
     int i;
+
     template<typename OStream>
-    friend OStream &operator<<(OStream &os, const my_type &c)
-    {
+    friend OStream &operator<<(OStream &os, const my_type &c) {
         return os << "[my_type i=" << c.i << "]";
     }
 };
 
-void user_defined_example()
-{
+void user_defined_example() {
     spdlog::info("user defined type: {}", my_type{14});
 }
 
 // Custom error handler. Will be triggered on log failure.
-void err_handler_example()
-{
+void err_handler_example() {
     // can be set globally or per logger(logger->set_error_handler(..))
-    spdlog::set_error_handler([](const std::string &msg) { printf("*** Custom log error handler: %s ***\n", msg.c_str()); });
+    spdlog::set_error_handler(
+            [](const std::string &msg) { printf("*** Custom log error handler: %s ***\n", msg.c_str()); });
 }
 
 // syslog example (linux/osx/freebsd)
 #ifndef _WIN32
+
 #include "spdlog/sinks/syslog_sink.h"
-void syslog_example()
-{
+
+void syslog_example() {
     std::string ident = "spdlog-example";
     auto syslog_logger = spdlog::syslog_logger_mt("syslog", ident, LOG_PID);
     syslog_logger->warn("This is warning that will end up in syslog.");
 }
+
 #endif
 
 // Android example.
@@ -233,3 +248,46 @@ void android_example()
 }
 
 #endif
+
+#include "spdlog/sinks/stdout_sinks.h"
+#include "spdlog/sinks/syslog_udp_sink.h"
+
+void replaceDefaultLogger() {
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
+//    auto console_sink = std::make_shared<spdlog::sinks::c>();
+    console_sink->set_level(spdlog::level::warn);
+    console_sink->set_pattern("[multi_sink_example] [%^%l%$] %v");
+
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/multisink.txt", true);
+    file_sink->set_level(spdlog::level::trace);
+
+
+    int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    struct sockaddr_in sockServerAddr;
+    memset(&sockServerAddr, 0, sizeof(struct sockaddr_in));
+    sockServerAddr.sin_family = AF_INET;
+    sockServerAddr.sin_addr.s_addr = inet_addr("10.0.0.5");  /* remote syslogd server ip */
+    sockServerAddr.sin_port = htons(514);
+
+    auto syslog_udp_sink = std::make_shared<spdlog::sinks::syslog_udp_sink_mt>(fd,
+                                                                               reinterpret_cast<const struct sockaddr *>(&sockServerAddr),
+                                                                               (socklen_t) sizeof(sockServerAddr),
+                                                                               std::string("my_host"),
+                                                                               std::string("my_app"),
+                                                                               (1 << 3));
+
+//    auto ptr = std::make_shared<Func>(std::initializer_list<std::string>{"foo", "bar", "baz"});
+
+//    auto logger = std::make_shared<spdlog::logger>("multi_sink", std::initializer_list<spdlog::sink_ptr>{console_sink, file_sink});
+    auto logger = std::make_shared<spdlog::logger>("multi_sink", std::initializer_list<spdlog::sink_ptr>{console_sink,
+                                                                                                         syslog_udp_sink});
+    logger->set_level(spdlog::level::debug);
+    logger->warn("this should appear in both console and file");
+    logger->info("this message should not appear in the console, only in the file");
+
+    spdlog::set_default_logger(logger);
+
+    spdlog::warn("this should also appear in both console and file");
+
+    spdlog::flush_on(spdlog::level::err);
+}
---
+++ include/spdlog/sinks/syslog_udp_sink.h
@@ -0,0 +1,156 @@
+//
+// Copyright(c) 2015 Gabi Melman.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#pragma once
+
+#ifndef SPDLOG_H
+#error "spdlog.h must be included before this file."
+#endif
+
+#include "spdlog/sinks/base_sink.h"
+
+#include <array>
+#include <string>
+#include <sys/socket.h>
+//#include <syslog.h>
+
+namespace spdlog {
+    namespace sinks {
+        namespace syslog_udp {
+            enum priority {
+//                EMERG = 0,
+//                ALERT = 1,
+                        CRIT = 2,
+                ERR = 3,
+                WARNING = 4,
+//                NOTICE = 5,
+                        INFO = 6,
+                DEBUG = 7,
+            };
+        }
+/**
+ * Sink that write to syslog using udp.
+ *
+ * Locking is not needed, as `sendmsg()` itself is thread-safe.
+ */
+        template<typename Mutex>
+        class syslog_udp_sink : public base_sink<Mutex> {
+        public:
+            //
+            explicit syslog_udp_sink(int fd, const struct sockaddr *sockServerAddr, socklen_t sockServerAddrLen,
+                                     std::string host = "", std::string ident = "", int syslog_facility = (1 << 3))
+                    : ident_(std::move(ident)),
+                      host_(std::move(host)),
+                      fd_(fd),
+                      sockServerAddrLen_(std::min(sockServerAddrLen, (socklen_t) sizeof(sockServerAddr_))),
+                      syslog_facility_(syslog_facility) {
+                priorities_[static_cast<size_t>(level::trace)] = syslog_udp::priority::DEBUG;
+                priorities_[static_cast<size_t>(level::debug)] = syslog_udp::priority::DEBUG;
+                priorities_[static_cast<size_t>(level::info)] = syslog_udp::priority::INFO;
+                priorities_[static_cast<size_t>(level::warn)] = syslog_udp::priority::WARNING;
+                priorities_[static_cast<size_t>(level::err)] = syslog_udp::priority::ERR;
+                priorities_[static_cast<size_t>(level::critical)] = syslog_udp::priority::CRIT;
+                priorities_[static_cast<size_t>(level::off)] = syslog_udp::priority::INFO;
+
+                memcpy(&sockServerAddr_, sockServerAddr, sockServerAddrLen_);
+            }
+
+            ~syslog_udp_sink() override {
+                if (fd_ == -1) {
+                    close(fd_);
+                }
+            }
+
+            syslog_udp_sink(const syslog_udp_sink &) = delete;
+
+            syslog_udp_sink &operator=(const syslog_udp_sink &) = delete;
+
+        protected:
+            void sink_it_(const details::log_msg &msg) override {
+                int prio = syslog_prio_from_level(msg);
+                int prival = syslog_facility_ + prio;
+
+                auto msgstr = fmt::to_string(msg.payload);
+
+                char tmpbuf[256];
+
+                char datestr[50];
+                time_t now = time(nullptr);
+                struct tm a_tm = {};
+                localtime_r(&now, &a_tm);
+                size_t len = strftime(datestr, sizeof(datestr) - 1, "%Y-%m-%dT%T.00Z", &a_tm);
+                datestr[len] = '\0';
+
+                int buflen = snprintf(tmpbuf, sizeof(tmpbuf), "<%d>1 %s %s %s[%d] - ",
+                                      prival,
+                                      datestr,
+                                      (host_.empty()) ? "-" : host_.c_str(),
+                                      (ident_.empty()) ? "-" : ident_.c_str(),
+                                      getpid()
+                );
+
+
+                iovec a_iovec[2];
+                a_iovec[0].iov_base = tmpbuf;
+                a_iovec[0].iov_len = std::min((size_t) buflen, sizeof(tmpbuf) - 1);
+
+                a_iovec[1].iov_base = (void *) msgstr.c_str();
+                a_iovec[1].iov_len = msgstr.length();
+
+                msghdr h;
+                memset(&h, 0, sizeof(h));
+                h.msg_iovlen = 2;
+                h.msg_iov = a_iovec;
+                h.msg_name = &sockServerAddr_;
+                h.msg_namelen = sockServerAddrLen_;
+
+                ssize_t res = sendmsg(fd_, &h, MSG_DONTWAIT);
+            }
+
+            void flush_() override {}
+
+        private:
+            std::array<int, 7> priorities_;
+            // must store the ident because the man says openlog might use the pointer as
+            // is and not a string copy
+            const std::string ident_;
+
+            const std::string host_;
+
+            int fd_;
+            socklen_t sockServerAddrLen_;
+            struct sockaddr sockServerAddr_;
+
+            int syslog_facility_;
+
+            //
+            // Simply maps spdlog's log level to syslog priority level.
+            //
+            int syslog_prio_from_level(const details::log_msg &msg) const {
+                return priorities_[static_cast<size_t>(msg.level)];
+            }
+        };
+
+        using syslog_udp_sink_mt = syslog_udp_sink<std::mutex>;
+        using syslog_udp_sink_st = syslog_udp_sink<details::null_mutex>;
+    } // namespace sinks
+
+// Create and register a syslog logger
+    template<typename Factory = default_factory>
+    inline std::shared_ptr<logger> syslog_udp_logger_mt(
+            const std::string &logger_name, const std::string &syslog_ident = "", int syslog_option = 0,
+            int syslog_facility = (1 << 3)) {
+        return Factory::template create<sinks::syslog_udp_sink_mt>(logger_name, syslog_ident, syslog_option,
+                                                                   syslog_facility);
+    }
+
+    template<typename Factory = default_factory>
+    inline std::shared_ptr<logger> syslog_udp_logger_st(
+            const std::string &logger_name, const std::string &syslog_ident = "", int syslog_option = 0,
+            int syslog_facility = (1 << 3)) {
+        return Factory::template create<sinks::syslog_udp_sink_st>(logger_name, syslog_ident, syslog_option,
+                                                                   syslog_facility);
+    }
+} // namespace spdlog
